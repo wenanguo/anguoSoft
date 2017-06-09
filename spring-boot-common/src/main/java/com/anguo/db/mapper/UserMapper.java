04ecd911d4ce788c5972a30d44f560fc70fcc475
Andrew Wen <wenanguo110@163.com>
2017-06-10 01:40:54 +0800

    项目第二阶段开始 使用spring-boot 重构整体框架 完成spring mvc 及 mabatis的整合

---
+++ spring-boot-common/src/main/java/com/anguo/db/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.anguo.db.mapper;
+
+import com.anguo.db.domain.User;
+import org.apache.ibatis.annotations.*;
+import java.util.List;
+
+
+@Mapper
+public interface UserMapper {
+
+    // 获取主键
+    @Insert("INSERT INTO user(name,password) VALUES (#{name}, #{password}) ")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(User user);
+
+    @Delete("DELETE FROM user WHERE id　= #{id}")
+    int delete(@Param("id") Integer id);
+
+    @Update("UPDATE user SET name = #{name}, password = #{password} WHERE id = #{id}")
+    int update(User user);
+
+    @Select("SELECT id, name, password FROM user WHERE id = #{id}")
+    @Results(id = "userMap", value = { @Result(column = "id", property = "id", javaType = Integer.class),
+            @Result(column = "name", property = "name", javaType = String.class),
+            @Result(column = "password", property = "password", javaType = String.class) })
+    User findById(Integer id);
+
+    @Select("SELECT * FROM user")
+    @ResultMap("userMap")
+    List<User> fingAll();
+
+
+    int insertUserXml(User user);
+}
