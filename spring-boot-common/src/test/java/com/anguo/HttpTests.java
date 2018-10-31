28e3c43be023d62237e757aff4fc1b4c3ce78531
Andrew Wen <wenanguo110@163.com>
2018-11-01 07:51:27 +0800

    加入单元测试

---
+++ spring-boot-common/src/test/java/com/anguo/HttpTests.java
@@ -0,0 +1,47 @@
+package com.anguo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpTests {
+
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    /**
+     * 参考 https://docs.spring.io/spring/docs/current/spring-framework-reference/testing.html#spring-mvc-test-framework
+     * get单元测试
+     * @throws Exception
+     */
+    @Test
+    public void getTest() throws Exception {
+        this.mockMvc.perform(get("/systemuser/rolelist")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.[0].roleName").value("Lee"));
+    }
+
+}
--- spring-boot-common/src/test/java/com/anguo/SpringBootCommonApplicationTests.java
+++ spring-boot-common/src/test/java/com/anguo/SpringBootCommonApplicationTests.java
@@ -1,7 +1,11 @@
 package com.anguo;
 
+import com.anguo.web.db.domain.User;
+import com.anguo.web.db.mapper.UserMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootCommonApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
+
 	@Test
 	public void contextLoads() {
+
+        User user=new User();
+
+        user.setPassword("111");
+        user.setName("insertUserXml:"+System.currentTimeMillis());
+
+        //int i=this.userMapper.insertUserXml(user);
+        int i=1;
+
+        Assert.assertEquals(i,1);
+
 	}
 
 }
